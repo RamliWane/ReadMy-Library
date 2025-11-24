7f08a64df43ce78f32a0d8a90363225f357fe131
RamliWane <ramlisilawane865@gmail.com>
2025-11-24 23:31:38 +0700

    new page for admin borrowpending

--- src/app/components/admin/SidebarAdmin.jsx
+++ src/app/components/admin/SidebarAdmin.jsx
@@ -60,6 +60,25 @@ export default function SidebarAdmin() {
                                 </button>
                             </a>
                         </div>
+                        <div role="button"
+                            className="flex items-center w-full p-0 leading-tight transition-all rounded-lg outline-none bg-blue-gray-50/50 text-start text-blue-gray-700 hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900">
+                            <a href="./dashboard" onClick={() => setIsOpen(false)}>
+                                <button type="button"
+                                    className="flex cursor-pointer items-center justify-between w-full p-3 font-sans text-xl antialiased font-semibold leading-snug text-left transition-colors border-b-0 select-none border-b-blue-gray-100 text-blue-gray-900">
+                                    <div className="grid mr-4 place-items-center">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+                                            fill="currentColor" className="w-5 h-5">
+                                            <path fillRule="evenodd"
+                                                d="M2.25 2.25a.75.75 0 000 1.5H3v10.5a3 3 0 003 3h1.21l-1.172 3.513a.75.75 0 001.424.474l.329-.987h8.418l.33.987a.75.75 0 001.422-.474l-1.17-3.513H18a3 3 0 003-3V3.75h.75a.75.75 0 000-1.5H2.25zm6.04 16.5l.5-1.5h6.42l.5 1.5H8.29zm7.46-12a.75.75 0 00-1.5 0v6a.75.75 0 001.5 0v-6zm-3 2.25a.75.75 0 00-1.5 0v3.75a.75.75 0 001.5 0V9zm-3 2.25a.75.75 0 00-1.5 0v1.5a.75.75 0 001.5 0v-1.5z"
+                                                clipRule="evenodd"></path>
+                                        </svg>
+                                    </div>
+                                    <p className="block mr-auto font-sans text-base antialiased font-normal leading-relaxed text-blue-gray-900">
+                                        Dashboard
+                                    </p>
+                                </button>
+                            </a>
+                        </div>
                     </div>
 
                     <div className="relative block w-full">
@@ -84,19 +103,21 @@ export default function SidebarAdmin() {
                         </div>
                     </div>
 
-                    <div role="button"
-                        className="flex cursor-pointer items-center w-full p-3 rounded-lg hover:bg-blue-gray-50 hover:text-blue-gray-900"
-                        onClick={() => setIsOpen(false)}>
-                        <div className="grid mr-4 place-items-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
-                                fill="currentColor" className="w-5 h-5">
-                                <path fillRule="evenodd"
-                                    d="M6.912 3a3 3 0 00-2.868 2.118l-2.411 7.838a3 3 0 00-.133.882V18a3 3 0 003 3h15a3 3 0 003-3v-4.162c0-.299-.045-.596-.133-.882l-2.412-7.838A3 3 0 0017.088 3H6.912zm13.823 9.75l-2.213-7.191A1.5 1.5 0 0017.088 4.5H6.912a1.5 1.5 0 00-1.434 1.059L3.265 12.75H6.11a3 3 0 012.684 1.658l.256.513a1.5 1.5 0 001.342.829h3.218a1.5 1.5 0 001.342-.83l.256-.512a3 3 0 012.684-1.658h2.844z"
-                                    clipRule="evenodd"></path>
-                            </svg>
+                    <a href="./borrowpending">
+                        <div role="button"
+                            className="flex cursor-pointer items-center w-full p-3 rounded-lg hover:bg-blue-gray-50 hover:text-blue-gray-900"
+                            onClick={() => setIsOpen(false)}>
+                            <div className="grid mr-4 place-items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+                                    fill="currentColor" className="w-5 h-5">
+                                    <path fillRule="evenodd"
+                                        d="M6.912 3a3 3 0 00-2.868 2.118l-2.411 7.838a3 3 0 00-.133.882V18a3 3 0 003 3h15a3 3 0 003-3v-4.162c0-.299-.045-.596-.133-.882l-2.412-7.838A3 3 0 0017.088 3H6.912zm13.823 9.75l-2.213-7.191A1.5 1.5 0 0017.088 4.5H6.912a1.5 1.5 0 00-1.434 1.059L3.265 12.75H6.11a3 3 0 012.684 1.658l.256.513a1.5 1.5 0 001.342.829h3.218a1.5 1.5 0 001.342-.83l.256-.512a3 3 0 012.684-1.658h2.844z"
+                                        clipRule="evenodd"></path>
+                                </svg>
+                            </div>
+                            Ajuan Pinjam
                         </div>
-                        Ajuan Pinjam
-                    </div>
+                    </a>
 
                     <div role="button"
                         className="flex cursor-pointer items-center w-full p-3 rounded-lg hover:bg-blue-gray-50 hover:text-blue-gray-900"
