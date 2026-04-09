9b0f147fef33be114cd1a8ad139833dc96ffd020
RamliWane <ramlisilawane865@gmail.com>
2026-04-09 22:48:24 +0700

    new page katalogclone

---
+++ src/app/katalogclone/page.jsx
@@ -0,0 +1,66 @@
+import Sidebar from "../components/Sidebar";
+import NavbarClone from "../components/NavbarClone";
+import SidebarClone from "../components/SideBarClone";
+import SearchBar from "../components/SearchBar";
+import FilterBook from "../components/FilterBook";
+import MasanryColumnBook from "../components/MasanryColumnBook";
+import HomeFooter from "../components/HomeFooter";
+
+
+export default function Katalog() {
+    return (
+        <div className="h-screen flex flex-col overflow-y-auto overflow-x-hidden text-black">
+            <NavbarClone />
+            <div className="flex flex-1 overflow-hidden">
+                <SidebarClone className="w-64 flex-0" />
+
+                <div className="grid overflow-y-auto no-scrollbar grid-cols-[1.7fr_1.7fr] grid-rows-[1fr_1fr] justify-center" style={{
+                    gridTemplateAreas:
+                        `
+              "header header"
+              "main main"
+            `
+                }}>
+                    <div className="flex-1 flex-wrap items-center animate-slide-up animate-delay-200 [grid-area:header]">
+                        <div className=" flex mt-2 w-full h-auto">
+                            <section className="flex-1">
+                                <div className="flex justify-start flex-col flex-wrap">
+                                    <div className="flex flex-col text-start ml-5 mt-4">
+                                        <h1
+                                            className="text-xl lg:text-xl text-black"
+                                            style={{ fontFamily: "'Happy Monkey', cursive" }}
+                                        >
+                                            <span className="text-[#6DC700]">Katalog Buku</span>
+                                        </h1>
+                                        <p
+                                            className="text-[14px] text-black"
+                                            style={{ fontFamily: "'Hanken Grotesk', sans-serif" }}
+                                        >
+                                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                                        </p>
+                                    </div>
+                                    <div className="pl-5">
+                                        <SearchBar />
+                                    </div>
+                                    <div className="pl-5">
+                                        <FilterBook />
+                                    </div>
+
+                                </div>
+                                <hr className="border-t border-black my-4 mx-5" />
+
+                                <div className="mt-5 m-10">
+                                    <MasanryColumnBook />
+                                </div>
+                                <div className="mb-10">
+                                    <HomeFooter />
+                                </div>
+                            </section>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    )
+}
