63301573ec93308e6957e5b076f68b7c1927a64f
RamliWane <ramlisilawane865@gmail.com>
2026-04-14 00:15:06 +0700

    new component booklist clone

--- src/lib/navigationitems.js
+++ src/lib/navigationitems.js
@@ -162,7 +162,7 @@ export const AdminNavigation = [
   },
   {
     label: "Book List",
-    href: "/booklist",
+    href: "/booklistclone",
     icon: (
        <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 8v4l3 3" />
